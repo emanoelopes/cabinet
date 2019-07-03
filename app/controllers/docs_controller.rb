4a0c08a65aee319e38ce5efea69751ac1c597d77
Emanoel Lopes <emanoelopes@gmail.com>
2019-07-03 16:47:16 -0300

    Association between users and docs

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -26,7 +26,7 @@ class DocsController < ApplicationController
   end
 
   def create
-    @doc = Doc.new(doc_params)
+    @doc = current_user.docs.build(doc_params)
     if @doc.save
         redirect_to @doc
     else
@@ -35,7 +35,7 @@ class DocsController < ApplicationController
   end
 
   def new
-    @doc = Doc.new
+    @doc = current_user.docs.build
   end
 
   private
